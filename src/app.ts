310a6384ccdd01b1051b3335e82f2c95904ca06a
joseignaciostassi <joseignaciostassi@gmail.com>
2024-06-17 11:25:43 -0300

    Se agrego delete, sanitezedInput y se agregaron returns

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import { Localidad } from "./classes/localidades/localidad.js"
 
 const app = express()
@@ -27,42 +27,77 @@ const localidades = [
   )
 ]
 
+function sanitizeLocalidadInput(req: Request, res: Response, next: NextFunction) {
+  
+  req.body.sanitizedInput = {
+    id: req.body.id, 
+    nombre: req.body.nombre, 
+    provincia: req.body.provincia, 
+    codpostal: req.body.codpostal
+  }
+  // Faltan validaciones aca de otras cosas
+
+  Object.keys(req.body.sanitizedInput).forEach(key => {
+    if(req.body.sanitizedInput[key] === undefined){
+      delete req.body.sanitizedInput[key]
+      }
+  })
+  next()
+}
 
 app.get('/api/localidades', (req, res) => {
   res.json(localidades)
 })
 
 app.get('/api/localidades/:id', (req, res) => {
-  const localidad = localidades.find(localidad => localidad.id === parseInt(req.params.id))
+  const localidad = localidades.find((localidad) => localidad.id === parseInt(req.params.id))
   if (!localidad) {
-    res.status(404).send('Localidad no encontrada')
+    return res.status(404).send({message: 'Localidad no encontrada'})
   }
   res.json(localidad)
 })
 
-app.post('/api/localidades', (req, res) => {
-  const {id, nombre, provincia, codpostal} = req.body
-  const localidad = new Localidad(id, nombre, provincia, codpostal)
+app.post('/api/localidades', sanitizeLocalidadInput, (req, res) => {
+  const input = req.body.sanitizedInput
+  const localidad = new Localidad(input.id, input.nombre, input.provincia, input.codpostal)
+
   localidades.push(localidad)
-  res.status(201).send({
-    message: 'Localidad creada',})
+  return res.status(201).send({message: 'Localidad creada', data: localidad})
 })
 
-app.put('/api/localidades/:id', (req, res) => {
+app.put('/api/localidades/:id', sanitizeLocalidadInput, (req, res) => {
   const localidadIdx = localidades.findIndex((localidad) => localidad.id === parseInt(req.params.id))
   
-  if (!localidadIdx) {
-    res.status(404).send('Localidad no encontrada')
+  if (localidadIdx===-1) {
+    return res.status(404).send('Localidad no encontrada')
   }
-  const input = {
-    id: req.body.id, 
-    nombre: req.body.nombre, 
-    provincia: req.body.provincia, 
-    codpostal: req.body.codpostal
-  }
-  localidades[localidadIdx] = {...localidades[localidadIdx], ...input}
+
+  localidades[localidadIdx] = {...localidades[localidadIdx], ...req.body.sanitizedInput}
   res.status(200).send({message: 'Localidad actualizada', data: localidades[localidadIdx]})
 })
+
+app.patch('/api/localidades/:id', sanitizeLocalidadInput, (req, res) => {
+  const localidadIdx = localidades.findIndex((localidad) => localidad.id === parseInt(req.params.id))
+  
+  if (localidadIdx===-1) {
+    return res.status(404).send('Localidad no encontrada')
+  }
+
+  localidades[localidadIdx] = {...localidades[localidadIdx], ...req.body.sanitizedInput}
+  res.status(200).send({message: 'Localidad actualizada', data: localidades[localidadIdx]})
+})
+
+app.delete('/api/localidades/:id', (req, res) => {
+  const localidadIdx = localidades.findIndex((localidad) => localidad.id === parseInt(req.params.id))
+  if (localidadIdx === -1) {
+    res.status(404).send({message: 'Localidad no encontrada'})
+  }
+  else{
+  localidades.splice(localidadIdx, 1)
+  res.status(200).send({message: 'Localidad eliminada'})
+  }
+})
+
 
 
 app.listen(3000, () => {
