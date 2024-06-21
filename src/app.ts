6ef5f98a90b20f0f7bc4a3d280158be7df836ba7
joseignaciostassi <joseignaciostassi@gmail.com>
2024-06-21 17:55:54 -0300

    Implementacion de ORM, aplicacion del ORM a localidad

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
+import "reflect-metadata"
 import express from "express"
 import { localidadRouter } from "./classes/localidades/localidad.routes.js"
+import { orm, syncSchema } from "./shared/db/orm.js"
+import { RequestContext } from "@mikro-orm/core"
 
 const app = express()
 app.use(express.json()) // Middleware para el req.body
+
+// Middleware del ORM
+app.use((req, res, next) => {
+  RequestContext.create(orm.em, next)
+})
+
 
 app.use('/api/localidades', localidadRouter)
 
 app.use((_, res) => {
   res.status(404).send({message:'Ruta no encontrada'})
 })
+
+await syncSchema()
 
 app.listen(3000, () => {
   console.log('Server started on port 3000')
--- src/shared/repository.ts
+++
@@ -1,7 +0,0 @@
-export interface Repository<T> {
-  findAll(): T[] | undefined
-  findOne(item: {id: number}): T | undefined    //Este id se modifique seguramente segun el autonumber de la BD
-  add(item: T): T | undefined
-  update(item: T): T | undefined
-  delete(item: {id: number}): T | undefined 
-}
